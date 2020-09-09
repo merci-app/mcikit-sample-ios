0bda854bdff68e8a7c9672a7cffbbde65ee03d25
Tiago Oliveira <tiago.oliveira@merci.app>
2020-09-09 17:21:38 -0300

    [Pod] Remove Sample folder

--- Sample/sign.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pushd "${TARGET_BUILD_DIR}"/"${PRODUCT_NAME}".app/Frameworks/MerciKit.framework/Frameworks
-for EACH in *.framework; do
-/usr/bin/codesign --force --deep --sign "${EXPANDED_CODE_SIGN_IDENTITY}" --entitlements "${TARGET_TEMP_DIR}/${PRODUCT_NAME}.app.xcent" --timestamp=none $EACH
-done
-popd
-echo "BUILD DIR ${TARGET_BUILD_DIR}"
